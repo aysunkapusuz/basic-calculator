0b8a525ad636e09c458fcb9d950ea7987372f9ad
Aysun <aysunkapusuz@gmail.com>
2022-05-22 08:43:37 -0600

    more code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const allClearButton = document.querySelector('[data-all-clear]')
+const deleteButton = document.querySelector('[data-delete]')
