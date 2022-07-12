9e2dc0de23b0a16325b047336727bc64d11d113f
Aysun <aysunkapusuz@gmail.com>
2022-07-12 09:26:24 -0600

    more code

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
+class Calculator {
+    constructor(preOperandTextElemet, currentOperandTextElemet){
+        this.preOperandTextElemet = preOperandTextElemet
+        this.currentOperandTextElemet = currentOperandTextElemet
+    }
+    clear (){
+        this.currentOperand = ''
+        this.currentOperand = ''
+
+    }
+
+    delete (){
+
+    }
+
+    appendNumber (number){
+
+    }
+
+    chooseOperation(operation){
+
+    }
+
+    compute(){
+
+    }
+
+    updateDisplay(){
+
+    }
+}
+
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const deleteButton = document.querySelector('[data-delete]')
 const equalButton = document.querySelector('[data-equals]')
 
-const preOprerandTextElemet = document.querySelector('[data-pre-operand')
-const currentOprerandTextElemet = document.querySelector('[data-current-operand')
+const preOperandTextElemet = document.querySelector('[data-pre-operand')
+const currentOperandTextElemet = document.querySelector('[data-current-operand')
 
