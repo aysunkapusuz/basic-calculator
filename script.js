b0f4de5e3cb581db16c1089982cc1b76c108789e
Aysun <aysunkapusuz@gmail.com>
2022-07-12 09:22:16 -0600

    more code

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const deleteButton = document.querySelector('[data-delete]')
 const equalButton = document.querySelector('[data-equals]')
 
+const preOprerandTextElemet = document.querySelector('[data-pre-operand')
+const currentOprerandTextElemet = document.querySelector('[data-current-operand')
+
